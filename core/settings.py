c15f3603b228fb62abdbef564248f3296b797a4b
BSIT606J2022 <116421244+BSIT606J2022@users.noreply.github.com>
2025-07-11 17:05:21 +0300

    created bookings app

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.authentication'
+    'apps.authentication',
+    'apps.bookings'
 ]
 
 MIDDLEWARE = [
