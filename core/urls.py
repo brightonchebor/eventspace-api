c4d2e29b70e2ad844497b1425f108006e166b6c8
Silas Ochieng <silaoketch2021@gmail.com>
2025-07-14 14:44:41 +0300

    connected spaces app to project core

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.authentication.urls')),
+    path('api/spaces/',include('apps.spaces.urls')),
 
 
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
