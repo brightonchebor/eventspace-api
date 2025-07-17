868538e548a81a1c1e930b534b801c9140137877
bajunii <haithamomar520@gmail.com>
2025-07-17 12:28:13 +0300

    Added views.py in notifications app

--- apps/bookings/admin.py
+++ apps/bookings/admin.py
@@ -3,7 +3,7 @@ from .models import Event
 # apps/bookings/admin.py
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('event_name', 'organizer_name', 'start_datetime', 'status')
+    list_display = ('event_name', 'organizer_name', 'start_datetime', 'end_datetime', 'event_type', 'status', 'space')
     list_filter = ('status', 'event_type')
     search_fields = ('event_name', 'organizer_name', 'organizer_email')
 
