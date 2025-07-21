e618ac76af5e77e613290e34c2d5f462546dbb04
brightonchebor <cheborbrighton805@gmail.com>
2025-07-21 14:06:35 +0300

    added createspace api

--- apps/spaces/urls.py
+++ apps/spaces/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CreateSpaceView
 
 urlpatterns = [
-    path('create/', CreateSpaceView.as_view(), name='create-space'),
+    path('', ListSpacesView.as_view(), name='list-spaces'),
 ]
