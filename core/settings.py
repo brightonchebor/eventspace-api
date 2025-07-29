158fbec44877b4b0e14cd170155e8657ef5493ba
erickmburu <rickomburuh@gmail.com>
2025-07-29 15:02:03 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
-   
+       
     'DEFAULT_AUTHENTICATION_CLASSES': (
        
         'rest_framework_simplejwt.authentication.JWTAuthentication',
